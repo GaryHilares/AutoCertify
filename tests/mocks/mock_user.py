7c750c783e1c485532271bb2f273088d7f7418d6
Gary Hilares <dev.garyhilares@gmail.com>
2023-07-15 11:18:28 -0500

    Added unit tests for /account/register

--- tests/mocks/mock_user.py
+++ tests/mocks/mock_user.py
@@ -10,7 +10,7 @@ class MockUser(UserMixin):
     Mocks the `User` class.
     """
 
-    def __init__(self, id_: str, name: str, password: str) -> MockUser:
+    def __init__(self, id_: str, name: str, password: str, url: str | None) -> MockUser:
         """
         Data to use for the mock.
 
@@ -18,10 +18,13 @@ class MockUser(UserMixin):
             id_; Mocks a MongoDB's id.
             name: Mocks user's name.
             password: Mocks user's password hash.
+            url: Mock user's verified URL.
         """
+        super().__init__()
         self.id_ = id_
         self.name = name
         self.password = password
+        self.url = url
 
     def get_id(self: MockUser) -> str:
         """
@@ -31,6 +34,12 @@ class MockUser(UserMixin):
             The mocked MongoDB's id.
         """
         return self.id_
+
+    def save(self: MockUser) -> None:
+        """
+        Mocks the `save` function. No-op.
+        """
+        pass
 
     @staticmethod
     def get_by_name(name: str) -> MockUser | None:
@@ -43,8 +52,22 @@ class MockUser(UserMixin):
         if name == "someuser":
             # User's password is 1234
             return MockUser(
-                "abc",
+                "someid",
                 "someuser",
                 b"$2b$12$St2gvjcv1nzl.ZaDqHIhLO1gLNsoZ1MB7gmO8yrHigI0j7rXx6pUW",
+                None,
             )
         return None
+
+    @staticmethod
+    def create(name: str, password: str) -> MockUser:
+        """
+        Mocks the `create` function, creating a new `MockUser` and returning it.
+
+        Args:
+            name: The name of the user to create.
+            password: The password hash of the user to create.
+        Returns:
+            The newly created mock user (without id nor verified URL).
+        """
+        return MockUser(None, name, password, None)
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -14,7 +14,7 @@ def client():
 
 def test_login_view(mocker, client) -> None:
     """
-    Tests the login functionality (/account/login).
+    Tests the login functionality (located at /account/login).
 
     Args:
         mocker: A mocking interface provided by `pytest-mock`.
@@ -22,7 +22,7 @@ def test_login_view(mocker, client) -> None:
     Raises:
         AssertionError: If any of the tests fails.
     """
-    # Mock that always
+    # Mock required functions from the User class
     mocker.patch("app.models.user.User.get_by_name", wraps=MockUser.get_by_name)
 
     # Test that log in view is displayed correctly
@@ -45,6 +45,75 @@ def test_login_view(mocker, client) -> None:
         data={
             "name": "someuser",
             "password": "1234",
+        },
+    )
+    assert b"Success" in response.data
+
+
+def test_register_view(mocker, client) -> None:
+    """
+    Tests the register functionality (located at /account/register).
+
+    Args:
+        mocker: A mocking interface provided by `pytest-mock`.
+        client: A Flask test client provided by a `pytest`'s fixture.
+    Raises:
+        AssertionError: If any of the tests fails.
+    """
+    # Mock required functions from the User class
+    mocker.patch("app.models.user.User.create", wraps=MockUser.create)
+    mocker.patch("app.models.user.User.get_by_name", wraps=MockUser.get_by_name)
+
+    # Test that log in view is displayed correctly
+    response = client.get("/account/register")
+    assert b"Register" in response.data
+
+    # Test that user can't register with a password with invalid characters
+    response = client.post(
+        "/account/register",
+        data={
+            "name": "newuser",
+            "password": "μ's-is-the-best",
+        },
+    )
+    assert b"Error" in response.data
+
+    # Test that user can't register with a password that is too short
+    response = client.post(
+        "/account/register",
+        data={
+            "name": "newuser",
+            "password": "123",
+        },
+    )
+    assert b"Error" in response.data
+
+    # Test that user can't register with a password that is too short
+    response = client.post(
+        "/account/register",
+        data={
+            "name": "newuser",
+            "password": "12345678901234567890123456789012345678901234567890.50",
+        },
+    )
+    assert b"Error" in response.data
+
+    # Test that user can't register with username that already exists
+    response = client.post(
+        "/account/register",
+        data={
+            "name": "someuser",
+            "password": "1234",
+        },
+    )
+    assert b"Error" in response.data
+
+    # Test that user can register
+    response = client.post(
+        "/account/register",
+        data={
+            "name": "newuser",
+            "password": "newpassword",
         },
     )
     assert b"Success" in response.data
